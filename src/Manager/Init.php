e00455bb97688d7326c75cda7c2a272a1d5f3b19
smallville <abutimartin778@gmail.com>
2024-11-21 21:44:51 +0300

    c1

--- src/Manager/Init.php
+++ src/Manager/Init.php
@@ -1,3 +1,7 @@
 <?php
-require ("CommandLine.php");
-require ("smv-config.php");
+require("CommandLine.php");
+if (!defined('BASE_DIR')) {
+    define('BASE_DIR', __DIR__ . '/');
+}
+if (file_exists(BASE_DIR . 'smv-config.php'))
+    require_once BASE_DIR . "smv-config.php";
