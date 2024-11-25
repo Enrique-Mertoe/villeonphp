09adcfed8ca5eaed36bb398a0d42391a4a0a7924
Enrique-mertoe <abutimartin778@gmail.com>
2024-11-25 03:23:28 +0300

    c2

--- src/Database/VilleonSQL/DataTypes/DataTypes.php
+++ src/Database/VilleonSQL/DataTypes/DataTypes.php
@@ -1,28 +1,46 @@
 <?php
 
 namespace Villeon\Database\VilleonSQL\DataTypes;
+
+/**
+ * Class DataTypes
+ *
+ * This class provides static methods and properties to define database column types
+ * and generate corresponding objects with optional configurations.
+ *
+ * It acts as a factory for creating specific data type objects with additional
+ * parameters (e.g., length, binary, options) that can be used to define database schemas.
+ */
 class DataTypes
 {
-    // Static string constants for types
     /**
-     * @var string
+     * @var string Represents a generic string data type.
      */
-    public static string $STRING = "STRING";
-    /**
-     * @var string
-     */
-    public static string $INT = "INT";
-    /**
-     * @var string
-     */
-    public static string $BOOL = "BOOL";
+    public const STRING = "STRING";
 
     /**
-     * Return a new StringDataType object with additional SQL parameters.
-     * @param int|null $length
-     * @param bool $binary
-     * @param array $options
-     * @return StringDataType
+     * @var string Represents a generic integer data type.
+     */
+    public const INT = "INT";
+
+    /**
+     * @var string Represents a generic boolean data type.
+     */
+    public const BOOL = "BOOL";
+
+    /**
+     * Generate a StringDataType object for defining string columns in the database.
+     *
+     * @param int|null $length The maximum length of the string (default: 255).
+     * @param bool $binary Whether the string should be stored as binary data (default: false).
+     * @param array $options Additional SQL parameters for customization (default: empty array).
+     *
+     * @return StringDataType An object representing the string data type.
+     *
+     * Example:
+     * ```php
+     * $stringColumn = DataTypes::STRING(100, false, ['charset' => 'utf8']);
+     * ```
      */
     public static function STRING(?int $length = 255, bool $binary = false, array $options = []): StringDataType
     {
@@ -30,13 +48,19 @@ class DataTypes
     }
 
     /**
-     * Return a new IntegerDataType object with additional SQL parameters.
-     * @param int|null $length
-     * @return IntegerDataType
+     * Generate an IntegerDataType object for defining integer columns in the database.
+     *
+     * @param int|null $length The maximum display width of the integer (default: 11).
+     *
+     * @return IntegerDataType An object representing the integer data type.
+     *
+     * Example:
+     * ```php
+     * $integerColumn = DataTypes::INTEGER(10);
+     * ```
      */
     public static function INTEGER(?int $length = 11): IntegerDataType
     {
         return new IntegerDataType($length, false);
     }
 }
-
